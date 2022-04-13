3fe02d563b4aa85a1914c4869242381d068de857
Ana Carolina Costa Cesar <anaipva@gmail.com>
2022-04-13 15:01:30 -0300

    Menu de escolhas feito

---
+++ TP2/main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(void) {
+    int resposta = 0;
+    FILE *arquivo = NULL;
+    while (resposta != 4)
+    {
+        printf("\n\nMENU - Escolha uma das opções abaixo    \n\n");
+        printf("'1' - Gera ABP (Arvore Binária de Pesquisa)\n");
+        printf("'2' - Calcula Fator de Balanceamento\n");
+        printf("'3' - Imprime ABP\n");
+        printf("'4' - Sair do programa\n\n");
+        scanf("%d", &resposta);
+
+        if (resposta == 1){
+            // arquivo = fopen("Videos.txt", "r");
+            // if(arquivo == NULL){
+            //     printf("Erro ao abrir o arquivo\n");
+            // }
+            // else{
+            //     printf("O arquivo foi encontrado...\n");
+            
+            // }
+            printf("\nGerando árvore...\n");
+        }
+        if (resposta == 2){
+            printf("\nCalculando fator de balanceamento...\n");
+        }
+        if (resposta == 3){
+            printf("\nImprimindo árvore...\n");
+        }
+        if (resposta == 4){
+            printf("\nSaindo...\n");
+            break;  
+            // fclose(arquivo);
+        }
+    }
+    
+	return 0;
+}
